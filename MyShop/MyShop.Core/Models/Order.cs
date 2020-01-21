2583d80bd5af921c86d7151f1e23b6dc611d2099
Fernando Martinez <cfmartinez99@gmail.com>
2020-01-21 09:38:04 -0600

    linked customer to orders

--- MyShop/MyShop.Core/Models/Order.cs
+++ MyShop/MyShop.Core/Models/Order.cs
@@ -17,6 +17,7 @@ namespace MyShop.Core.Models
         public string Email { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
+        public string State { get; set; }
         public string ZipCode { get; set; }
         public string OrderStatus { get; set; }
         public virtual ICollection<OrderItem> OrderItems { get; set; }
--- MyShop/MyShop.WebUI/Controllers/BasketController.cs
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -12,11 +12,13 @@ namespace MyShop.WebUI.Controllers
     {
         IBasketService basketService;
         IOrderService orderService;
+        IRepository<Customer> customers;
 
-        public BasketController(IBasketService BasketService, IOrderService OrderService)
+        public BasketController(IBasketService BasketService, IOrderService OrderService, IRepository<Customer> Customers)
         {
             this.basketService = BasketService;
             this.orderService = OrderService;
+            this.customers = Customers;
         }
 
         // GET: Basket
@@ -45,16 +47,40 @@ namespace MyShop.WebUI.Controllers
             return PartialView(basketSummary);
         }
 
+        [Authorize] 
         public ActionResult CheckOut()
         {
-            return View();
+            Customer customer = customers.Collection().FirstOrDefault(c => c.Email == User.Identity.Name);
+
+            if(customer != null)
+            {
+                Order order = new Order()
+                {
+                    Email = customer.Email,
+                    City = customer.City,
+                    State = customer.State,
+                    Street = customer.Street,
+                    FirstName = customer.FirstName,
+                    SurName = customer.LastName,
+                    ZipCode = customer.ZipCode
+                };
+
+                return View(order);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+            
         }
 
         [HttpPost]
+        [Authorize]
         public ActionResult CheckOut(Order order)
         {
             var BasketItems = basketService.GetBasketItems(this.HttpContext);
             order.OrderStatus = "Order Created";
+            order.Email = User.Identity.Name;
 
             //process payment
 
